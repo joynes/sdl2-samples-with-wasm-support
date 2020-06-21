5b5b2ae8acbd0141ce50065dee7d17733a91b370
Johannes Kählare <joynez11@gmail.com>
2020-06-21 15:00:53 +0200

    Simple game

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # sdl2-samples-with-wasm-support
-Simple SDL2 with WebAssembly (wasm) support
+Simple SDL2 with WebAssembly (wasm) support. Tested on macOS Catalina / Chrome.
 
 ```bash
-# Build and run for macOS
-clang -l SDL2-2.0.0 render_color_using_sdl2_wasm.c && ./a.out
+# Install sdl2 and emscripten with brew or by other means
 
-# Build and run for chrome (no_browser workaround for issue with emrun)
-emcc render_color_using_sdl2_wasm.c -s WASM=1 -s USE_SDL=2 -o render_color_using_sdl2_wasm/render_color_using_sdl2_wasm.html && open http://0.0.0.0:6931/render_color_using_sdl2_wasm.html && emrun --no_browser render_color_using_sdl2_wasm/render_color_using_sdl2_wasm.html
+# Build and run for macOS
+./build_osx.sh <folder-name>
+
+# Build and run for chrome
+./build_web.sh <folder-name>
 ```
---
+++ simple_game_using_sdl2_wasm.c
@@ -0,0 +1,108 @@
+#define GL_SILENCE_DEPRECATION
+
+#include <math.h>
+#include <SDL2/SDL.h>
+#ifdef __APPLE__
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/gl3.h>
+#endif
+#ifdef __EMSCRIPTEN__
+#include <SDL_opengles2.h>
+#include <emscripten.h>
+#endif
+
+struct Context { SDL_Window *window; double i; GLuint time; };
+
+int bail(int i) { if (i) SDL_Log("Error: %s, code %d\n", SDL_GetError(), i); exit(i); return 1; }
+int glbail(int i, int s) { char str[10000]; glGetShaderInfoLog(i, 10000, NULL, str); SDL_Log("GL: %s, code %x\n", str, glGetError()); exit(i); return 1; }
+void quit_on_glerr() { GLuint e; ((e = glGetError()) == GL_NO_ERROR) || bail(e); }
+void quit() { SDL_Quit(); }
+
+void step(void * _ctx) {
+  struct Context *ctx = (struct Context *)_ctx;
+  ctx->i++;
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) { event.type == SDL_QUIT && bail(0); }
+
+  glUniform1f(ctx->time, ctx->i/60.);
+  glClearColor(0., 0., 0, 1.);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+
+  SDL_GL_SwapWindow(ctx->window);
+}
+
+int main() {
+  SDL_Init(SDL_INIT_VIDEO) && bail(1);
+  atexit(quit);
+
+  struct Context ctx = {0};
+
+  (ctx.window = SDL_CreateWindow("Render color", 0, 0, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE)) || bail(2);
+  SDL_GLContext glcontext = SDL_GL_CreateContext(ctx.window);
+  SDL_GL_SetSwapInterval(1);
+
+  GLfloat vertices[] = {
+    -1., -1.,
+    1., -1.,
+    1., 1.,
+    -1., 1.
+  };
+  GLuint buf;
+  glGenBuffers(1, &buf);
+  glBindBuffer(GL_ARRAY_BUFFER, buf);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 8, vertices, GL_STATIC_DRAW);
+
+  GLuint prg = glCreateProgram();
+
+  const char *vsrc =
+"\
+attribute vec2 vertice;\
+varying vec2 coord;\
+void main() {\
+  gl_Position = vec4(vertice, 0., 1.);\
+  coord = (vertice + 1.)/2.;\
+}\
+";
+
+  GLuint vert = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vert, 1, &vsrc, NULL);
+  glCompileShader(vert);
+  GLint status; glGetShaderiv(vert, GL_COMPILE_STATUS, &status); (status == GL_TRUE) || glbail(vert, status);
+  glAttachShader(prg, vert);
+
+  char *fsrc =
+"\
+precision highp float;\
+varying vec2 coord;\
+uniform float time;\
+void main() {\
+  gl_FragColor = vec4((sin(coord.x + time)+1.)/2., (cos(coord.y + time*.9)+1.)/2., sin(coord.y+coord.x + time*.8), 1.);\
+}\
+";
+  GLuint frag = glCreateShader(GL_FRAGMENT_SHADER);
+#ifdef __APPLE__
+  fsrc = fsrc+22;
+#endif
+  glShaderSource(frag, 1, &fsrc, NULL);
+  glCompileShader(frag);
+  glGetShaderiv(frag, GL_COMPILE_STATUS, &status); (status == GL_TRUE) || glbail(frag, status);
+  glAttachShader(prg, frag);
+
+  glLinkProgram(prg);
+  glGetProgramiv(prg, GL_LINK_STATUS, &status); (status == GL_TRUE) || bail(4);
+  glUseProgram(prg);
+
+  GLuint vertices_attr = glGetAttribLocation(prg, "vertice");
+  glVertexAttribPointer(vertices_attr, 2, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(vertices_attr);
+
+  ctx.time = glGetUniformLocation(prg, "time");
+
+#ifdef __EMSCRIPTEN__
+  emscripten_set_main_loop_arg(step, &ctx, -1, 1);
+#else
+  while(1) step(&ctx);
+#endif
+  return 0;
+}
