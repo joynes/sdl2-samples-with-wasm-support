09388ae7550b137203830ca52178e84b5f4b1a2f
Johannes Kählare <joynez11@gmail.com>
2020-06-22 10:56:57 +0200

    Mouse input

--- simple_game_using_sdl2_wasm.c
+++ simple_game_using_sdl2_wasm.c
@@ -11,10 +11,11 @@
 #include <emscripten.h>
 #endif
 
-struct Context { SDL_Window *window; double i; GLuint time; };
+struct Obj { float x, y; };
+struct Context { SDL_Window *window; double i; GLuint time; struct Obj obj; };
 
 int bail(int i) { if (i) SDL_Log("Error: %s, code %d\n", SDL_GetError(), i); exit(i); return 1; }
-int glbail(int i, int s) { char str[10000]; glGetShaderInfoLog(i, 10000, NULL, str); SDL_Log("GL: %s, code %x\n", str, glGetError()); exit(i); return 1; }
+int glbail(int i) { char str[10000]; glGetShaderInfoLog(i, 10000, NULL, str); SDL_Log("GL: %s, code %x\n", str, glGetError()); exit(i); return 1; }
 void quit_on_glerr() { GLuint e; ((e = glGetError()) == GL_NO_ERROR) || bail(e); }
 void quit() { SDL_Quit(); }
 
@@ -22,7 +23,13 @@ void step(void * _ctx) {
   struct Context *ctx = (struct Context *)_ctx;
   ctx->i++;
   SDL_Event event;
-  while (SDL_PollEvent(&event)) { event.type == SDL_QUIT && bail(0); }
+  while (SDL_PollEvent(&event)) {
+    event.type == SDL_QUIT && bail(0);
+    if (event.type == SDL_MOUSEMOTION) {
+      SDL_MouseMotionEvent e = event.motion;
+      printf("%d\n", e.xrel);
+    }
+  }
 
   glUniform1f(ctx->time, ctx->i/60.);
   glClearColor(0., 0., 0, 1.);
@@ -34,12 +41,13 @@ void step(void * _ctx) {
 
 int main() {
   SDL_Init(SDL_INIT_VIDEO) && bail(1);
+  SDL_SetRelativeMouseMode(SDL_ENABLE);
   atexit(quit);
 
   struct Context ctx = {0};
 
   (ctx.window = SDL_CreateWindow("Render color", 0, 0, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE)) || bail(2);
-  SDL_GLContext glcontext = SDL_GL_CreateContext(ctx.window);
+  SDL_GL_CreateContext(ctx.window);
   SDL_GL_SetSwapInterval(1);
 
   GLfloat vertices[] = {
@@ -61,14 +69,14 @@ attribute vec2 vertice;\
 varying vec2 coord;\
 void main() {\
   gl_Position = vec4(vertice, 0., 1.);\
-  coord = (vertice + 1.)/2.;\
+  coord = vertice;\
 }\
 ";
 
   GLuint vert = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vert, 1, &vsrc, NULL);
   glCompileShader(vert);
-  GLint status; glGetShaderiv(vert, GL_COMPILE_STATUS, &status); (status == GL_TRUE) || glbail(vert, status);
+  GLint status; glGetShaderiv(vert, GL_COMPILE_STATUS, &status); (status == GL_TRUE) || glbail(vert);
   glAttachShader(prg, vert);
 
   char *fsrc =
@@ -76,17 +84,19 @@ void main() {\
 precision highp float;\
 varying vec2 coord;\
 uniform float time;\
+uniform float pos;\
 void main() {\
-  gl_FragColor = vec4((sin(coord.x + time)+1.)/2., (cos(coord.y + time*.9)+1.)/2., sin(coord.y+coord.x + time*.8), 1.);\
+  float s = .1;\
+  gl_FragColor = vec4(vec3(step(abs(coord.x-pos.x), s)*step(abs(coord.y-pos.y), s)), 1.);\
 }\
 ";
   GLuint frag = glCreateShader(GL_FRAGMENT_SHADER);
 #ifdef __APPLE__
   fsrc = fsrc+22;
 #endif
-  glShaderSource(frag, 1, &fsrc, NULL);
+  glShaderSource(frag, 1, (const char **)&fsrc, NULL);
   glCompileShader(frag);
-  glGetShaderiv(frag, GL_COMPILE_STATUS, &status); (status == GL_TRUE) || glbail(frag, status);
+  glGetShaderiv(frag, GL_COMPILE_STATUS, &status); (status == GL_TRUE) || glbail(frag);
   glAttachShader(prg, frag);
 
   glLinkProgram(prg);
